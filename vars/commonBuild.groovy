3c1c05118696a4a0f7810b05db8e9af30f14b914
Aniruddha Das <90250998+aniruddh030502@users.noreply.github.com>
2025-06-01 22:37:15 +0530

    Update commonBuild.groovy

--- vars/commonBuild.groovy
+++ vars/commonBuild.groovy
@@ -2,6 +2,7 @@ def call(){
 stage('checkout'){
 // checkout scm
   echo "hello universe"
+  git branch "main" ,url "https://github.com/aniruddh030502/a2abin"
 }
 stage('Unit Tests'){
 echo "Running unit tests... "
